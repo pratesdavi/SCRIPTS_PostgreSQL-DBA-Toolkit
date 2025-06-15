b8ea74ac9b99ba4d32480219c2dc5fed93847a3c
Davi Prates <126431158+pratesdavi@users.noreply.github.com>
2025-06-14 22:59:38 -0300

    Update atualizacao_categoria.sql

--- atualizacao_categoria.sql
+++ atualizacao_categoria.sql
@@ -1,6 +1,17 @@
---- ## ATULIZAÇÃO DE BU ## ---
--- ## TV
-update nome_tabela nt
-set categoria = nome_categoria 
-from tabela_itens ti
-where nt.id_item = ti.id_item
+-- ###########################################################################
+-- ## 🔄 ATUALIZAÇÃO DE CATEGORIA VIA DIMENSÃO DE ITENS                    ##
+-- ## Atualiza a coluna 'categoria' na tabela 'nome_tabela' com os valores ##
+-- ## da coluna 'nome_categoria' da tabela 'tabela_itens', baseando-se na  ##
+-- ## correspondência pelo campo 'id_item'.                                ##
+-- ###########################################################################
+
+-- ✅ Sintaxe para PostgreSQL ou SQL Server:
+UPDATE nome_tabela nt
+SET categoria = nome_categoria 
+FROM tabela_itens ti
+WHERE nt.id_item = ti.id_item;
+
+-- ✅ Sintaxe equivalente para MySQL:
+UPDATE nome_tabela nt
+JOIN tabela_itens ti ON nt.id_item = ti.id_item
+SET nt.categoria = ti.nome_categoria;
